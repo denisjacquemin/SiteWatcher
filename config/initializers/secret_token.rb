610962b564fb37a128718638b7601a0d185d3c73
Denis Jacquemin <denis.jacquemin@gmail.com>
2011-10-16 17:39:51 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SiteWatcher::Application.config.secret_token = 'bc0420c747ab2e893c3751a113bdbc3f2bee0a99de279f44e5cb228b2b4f806d3bbfbaa94a8e71f5717168267d02701f7da37fb4ee4d03d76d153f129a281241'
