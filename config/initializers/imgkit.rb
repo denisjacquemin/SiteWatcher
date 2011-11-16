2806067092ba0f499694f20498430fcacab13227
Denis Jacquemin <denis.jacquemin@gmail.com>
2011-11-16 18:26:50 +0100

    back to initial config with IMGKit initializer

--- config/initializers/imgkit.rb
+++ config/initializers/imgkit.rb
@@ -1,3 +1,3 @@
 IMGKit.configure do |config|
-  config.wkhtmltoimage = Rails.root.join('bin', 'wkhtmltoimage-amd64').to_s if ENV['Rails.env'] == 'production'
+  config.wkhtmltoimage = Rails.root.join('bin', 'wkhtmltoimage-amd64').to_s if ENV['RACK_ENV'] == 'production'
 end
