f2cae0bcfa4192c644ea8a4b2580bca0a58fa86a
Denis Jacquemin <denis.jacquemin@gmail.com>
2011-11-16 21:03:42 +0100

    remove IMGkit comf in production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -57,9 +57,4 @@ SiteWatcher::Application.configure do
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
-  
-  
-  IMGKit.configure do |config|
-    config.wkhtmltoimage = Rails.root.join('bin', 'wkhtmltoimage-amd64').to_s if ENV['RACK_ENV'] == 'production'
-  end
 end
