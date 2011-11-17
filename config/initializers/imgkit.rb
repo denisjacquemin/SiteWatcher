2d4be842b5ca8e3cc11b2490ad6bc6e83aef7c15
Denis Jacquemin <denis.jacquemin@gmail.com>
2011-11-17 06:27:44 +0100

    back to original config of IMGkit

--- config/initializers/imgkit.rb
+++ config/initializers/imgkit.rb
@@ -1,2 +1,3 @@
 IMGKit.configure do |config|
-  config.wkhtmltoimage = Rails.root.join('bin', 'wkhtmltoimage-amd64').to_s
+  config.wkhtmltoimage = Rails.root.join('bin', 'wkhtmltoimage-amd64').to_s 
+end
