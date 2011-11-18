69ecbd4956c34786039225399a65ca7159721dd6
Denis Jacquemin <denis.jacquemin@gmail.com>
2011-11-18 06:38:37 +0100

    carrierwave/fog config done

---
+++ config/initializers/carrier_wave.rb
@@ -0,0 +1,12 @@
+CarrierWave.configure do |config|
+  config.fog_credentials = {
+    :provider               => 'AWS',       # required
+    :aws_access_key_id      => ENV['S3_KEY'],       # required
+    :aws_secret_access_key  => ENV['S3_SECRET'],       # required
+    :region                 => 'us-east-1'  # optional, defaults to 'us-east-1'
+  }
+  config.fog_directory  = 'SiteWatcher'                     # required
+  config.fog_host       = 'https://assets.example.com'            # optional, defaults to nil
+  config.fog_public     = false                                   # optional, defaults to true
+  config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}  # optional, defaults to {}
+end
