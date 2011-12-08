78701b1d139ce84b55ee6c873841d97f61dcf192
Denis Jacquemin <denis.jacquemin@gmail.com>
2011-12-08 15:56:32 +0100

    refresh all sequencial

--- app/assets/javascripts/people.js
+++ app/assets/javascripts/people.js
@@ -1,15 +1,20 @@
-function refreshAllPeople() {
-    $('.refresh').each(function(){
-        $(this).click();
-    })
+
+function refreshAllPeople(links, index) {
+    if (index == links.length) return;
+
+    $(links[index]).hide();
+    $(links[index]).prev('img').show(); // img tag is a spinner
+
+    $.ajax({ url: $(links[index]).attr('href'),
+             success: function(data, status, xhr) {
+                $(links[index]).parent().prev('.title').html(data.title);
+                $(links[index]).show();
+                $(links[index]).prev('img').hide();
+                refreshAllPeople(links, ++index);
+             },
+             error: function(data, status, xhr) {
+                 alert(error);
+             }
+             
+    });
 }
-
-$('.refresh').on('click', function(event) {
-   $(this).hide();
-   $(this).prev('img').show(); 
-});
-
-$('.refresh').on('ajaxStop', function(event) {
-   $(this).show();
-   $(this).prev('img').hide(); 
-});
