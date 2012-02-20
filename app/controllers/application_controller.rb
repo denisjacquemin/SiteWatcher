a774ff7c71c10765fd59218012e8bd9a4a5894f3
Denis Jacquemin <denis.jacquemin@gmail.com>
2012-02-20 15:56:38 +0100

    don't render the layout if PJAX is working

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  
+  # https://gist.github.com/999482
+  # We don't want to render the layout if PJAX is working
+  def render(options = nil, extra_options = {}, &block)
+    if request.headers['X-PJAX'] == 'true'
+      options = {} if options.nil?
+      options[:layout] = false
+    end
+    super(options, extra_options, &block)
+  end
 end
