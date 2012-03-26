eaa848196fb39c4fb1571905a2d83175619bdbda
Denis Jacquemin <denis.jacquemin@gmail.com>
2012-03-26 18:49:20 +0200

    Fetchers refactored

--- lib/fetcher/linkedin.rb
+++ lib/fetcher/linkedin.rb
@@ -6,7 +6,7 @@ module Fetcher
         agent = Mechanize.new
         agent.keep_alive = false
     
-        # get linkedin homepage
+        # get homepage
         home_page = agent.get('http://www.linkedin.com/')
     
         # get search form
@@ -21,102 +21,40 @@ module Fetcher
         vcards = agent.page.search('.vcard')
         info = Information.new
         vcards.each_with_index do |vcard, index|
-          # fetch title and linkedin_profile_url
-          title = vcard.search('.vcard-basic .title').text().gsub("\n","").strip()
-          linkedin_profile_url = vcard.search('h2 strong a').attr('href').value()
-          # if linkedin_profile_url found => already_exist = true else false
-          
-          # TODO: use first_or_create!
-          
-          info = Information.by_user(person.user_id).where(:linkedin_url => linkedin_profile_url).first # check if the profile is already in db
-          if info.nil?
-            # if already_exist == false then fetch everything and create
-            
-            location = vcard.search('.location').text().gsub("\n","").strip()
-            industry = vcard.search('.industry').text().gsub("\n","").strip()
-            past = vcard.search('.past-content').text()
-            info = Information.new # create a new object if profile not yet in db
-            info.title = title
-            info.region = location
-            info.industry = industry
-            info.person_id = person.id
-            info.past = past
-            info.iscurrent = true
-            info.validated = true
-            info.linkedin_url = linkedin_profile_url
-
-            info.save
-            puts "#{index}. found new profile for #{person.firstname} #{person.lastname}: #{title}"
-
-          else
-            if info.title != title
-              info.old_title = info.title
-              info.title = title
-              info.region = location
-              info.industry = industry
-              info.person_id = person.id
-              info.past = past
-              info.iscurrent = true
-              info.save
-              puts "#{index}. profile updated for #{person.firstname} #{person.lastname}: new title is: #{title}, old title is #{info.old_title}"
-            end 
-          end
-        end
-        if person.processed != true
-          person.processed = true
-          person.save
-        end
-        result = Result.new
-        if vcards.size > 1
-          result.title = "found #{vcards.size} profiles(s)"
-          result.has_profiles = true
-          return result
-        elsif vcards.size == 1
-          result.title = info.title
-          result.has_profiles = true 
-          return result       
-        end
+        
+        # fetch all information
+        jobtitle = vcard.search('.vcard-basic .title').text().gsub("\n","").strip()
+        linkedin_profile_url = vcard.search('h2 strong a').attr('href').value()
+        location = vcard.search('.location').text().gsub("\n","").strip()
+        industry = vcard.search('.industry').text().gsub("\n","").strip()
+        past = vcard.search('.past-content').text()
+        
+        profile = Profile.where(:url => linkedin_profile_url).first
+        profile = Profile.new(:url => linkedin_profile_url, :person_id => person.id, :profile_type_id => 1) if profile.nil?
+        
+        profile.elements << Element.new(:label => 'jobtitle', :value => jobtitle)
+        profile.elements << Element.new(:label => 'location', :value => location)
+        profile.elements << Element.new(:label => 'industry', :value => industry)
+        profile.elements << Element.new(:label => 'past',     :value => past)
+        profile.save
+        
+        puts "#{index}. Profile LinkedIn for #{person.firstname} #{person.lastname}: #{title}"
+        
       rescue Net::HTTP::Persistent::Error
-        puts "Net::HTTP::Persistent::Error "
-        result = Result.new
-        result.has_error = true
-        result.error_message = "Net::HTTP::Persistent::Error"
-        return result
+        puts "Net::HTTP::Persistent::Error for #{person.firstname} #{person.lastname}"
       rescue SocketError
-        puts "SocketError"
-        result = Result.new
-        result.has_error = true
-        result.error_message = "SocketError"
-        return result
+        puts "SocketError for #{person.firstname} #{person.lastname}"
       rescue Timeout::Error
-        puts "caught Timeout::Error !"
-        result = Result.new
-        result.has_error = true
-        result.error_message = "Timeout::Error"
-        return result
+        puts "caught Timeout::Error for #{person.firstname} #{person.lastname} !"
       rescue Mechanize::ResponseCodeError => e
           case e.response_code
             when "502"
-              puts "  caught Net::HTTPBadGateway !"
-              result = Result.new
-              
-              result.has_error = true
-              result.error_message = "HTTPBadGateway"
+              puts "  caught Net::HTTPBadGateway for #{person.firstname} #{person.lastname}!"
               return result
             when "404"
               puts "  caught Net::HTTPNotFound for #{person.firstname} #{person.lastname}!"
-              result = Result.new
-              
-              result.has_error = true
-              result.error_message = "Net::HTTPNotFound"
-              return result
             else
-              puts "caught Exception !" + e.response_code
-              result = Result.new
-              
-              result.has_error = true
-              result.error_message = "Exception"
-              return result
+              puts "caught Exception for #{person.firstname} #{person.lastname}" + e.response_code
           end
       end
     end
